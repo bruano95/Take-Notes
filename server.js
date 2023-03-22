b2b6711af7964c928c0d2217a4f1e106196d6a1e
bruano95 <bruano@uw.edu>
2023-03-22 09:45:47 -0700

    updated port for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const route = require('./routes');
 
-const PORT = 3003;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
