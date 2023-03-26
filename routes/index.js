d79647ee0a4d7292bbd5e756d7d13f876cc1ce44
bruano95 <bruano@uw.edu>
2023-03-25 17:09:32 -0700

    fixed path

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const htmlRoutes = require('./htmlRoutes');
-const apiRoutes = require('./apiRoutes');
+const htmlRoutes = require('./htmlroutes');
+const apiRoutes = require('./apiroutes');
 
 const app = express()
 
